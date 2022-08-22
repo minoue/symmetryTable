190597fe24a2c22f506962fb0de167dfc7945aa7
Michi Inoue <mtkinoue@gmail.com>
2022-08-22 07:32:50 +0000

    Update plugin registration

--- src/pluginMain.cpp
+++ src/pluginMain.cpp
@@ -1,18 +1,40 @@
 #include "symmetryTable.hpp"
 #include <maya/MFnPlugin.h>
 
-static const char* cmdName = "createSymmetryTable";
+static const char* const cmdName = "createSymmetryTable";
+static const char* const version = "0.0.3";
+static const char* const author = "Michitaka Inoue";
 
 MStatus initializePlugin(MObject mObj)
 {
-    MFnPlugin fnPlugin(mObj, "Michitaka Inoue", "0.0.2", "Any");
-    fnPlugin.registerCommand(cmdName, SymmetryTable::creator);
+    MStatus status;
+
+    std::string version_str(version);
+    std::string compile_date_str(__DATE__);
+    std::string compile_time_str(__TIME__);
+    std::string ver(version_str + " / " + compile_date_str + " / " + compile_time_str); 
+
+    MFnPlugin fnPlugin(mObj, author, ver.c_str(), "Any");
+    
+    status = fnPlugin.registerCommand(cmdName, SymmetryTable::creator);
+    if (!status) {
+        status.perror("registerCommand");
+        return status;
+    }
     return MS::kSuccess;
 }
 
 MStatus uninitializePlugin(MObject mObj)
 {
+    MStatus status;
+
     MFnPlugin fnPlugin(mObj);
-    fnPlugin.deregisterCommand(cmdName);
+    status = fnPlugin.deregisterCommand(cmdName);
+
+    if (!status) {
+        status.perror("deregisterCommand");
+        return status;
+    }
+
     return MS::kSuccess;
 }
